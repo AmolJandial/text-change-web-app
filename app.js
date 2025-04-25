fb450d8c2736d4c8bcbbd111b3c0f885c22341d3
amol jandial <amoljandial41@gmail.com>
2025-04-25 14:25:07 +0530

    finished app

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const containerEl = document.querySelector(".container");
 
-const fields = ["Web Developer, Flutter Developer, React Developer"];
+const fields = ["Web Developer", "Flutter Developer", "React Developer"];
 
-containerEl.innerHTML = `
-<h1>I am a web developer</h1>
+let index = 0;
+let characterIndex = 0;
+
+createElement();
+
+function createElement() {
+  characterIndex++;
+  containerEl.innerHTML = `
+<h1>I am a ${fields[index].slice(0, characterIndex)}</h1>
 `;
+  if (characterIndex == fields[index].length) {
+    characterIndex = 0;
+    index++;
+  }
+  if (index == fields.length) {
+    index = 0;
+  }
+
+  setTimeout(createElement, 400);
+}
