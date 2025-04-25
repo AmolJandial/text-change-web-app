a0bf503ea4b58dc072bb0a9721a6a73091aded76
amol jandial <amoljandial41@gmail.com>
2025-04-25 14:12:39 +0530

    created project

---
+++ app.js
@@ -0,0 +1,7 @@
+const containerEl = document.querySelector(".container");
+
+const fields = ["Web Developer, Flutter Developer, React Developer"];
+
+containerEl.innerHTML = `
+<h1>I am a web developer</h1>
+`;
